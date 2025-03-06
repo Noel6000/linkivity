26b25c16ce77f07596457ea6619bfeaa9ee1620c
Noel6000 <noelsantiago.briand@gmail.com>
2025-03-06 13:52:43 +0100

    Update tests.py

--- pages/tests.py
+++ pages/tests.py
@@ -4,4 +4,4 @@ import numpy as np
 import random
 import time
 
-stb.true_or_false(f"To print 'Hello world', you would write \"print('Hello World')\"", False)
+stb.true_or_false(f"To print 'Hello world', you would write \"print(\"Hello World\")\"", True)
