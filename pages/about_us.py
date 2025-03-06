43bc5f44eafc0ca4840854448cb2fe9b39b4e35d
Noel6000 <noelsantiago.briand@gmail.com>
2025-03-06 11:54:21 +0100

    Update about_us.py

--- pages/about_us.py
+++ pages/about_us.py
@@ -6,6 +6,8 @@ from openai import OpenAI
 
 st.header("About us")
 st.write("We are Baltasar, Andrés, Michael, Jiajia, Lilian, and Noël, a group of 8th grade students that have developed an alternative to linkedin for the Eureka Starter Days.")
+st.image("group.png")
+
 
 isClicked = st.button("mainpage", use_container_width=True)
 
