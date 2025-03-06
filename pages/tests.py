57f332dc62eb2c8585c42f3d96895bb19f22e29d
Noel6000 <noelsantiago.briand@gmail.com>
2025-03-06 14:05:19 +0100

    Update tests.py

--- pages/tests.py
+++ pages/tests.py
@@ -6,19 +6,29 @@ import time
 
 correct = 0
 questions = 0
-if stb.true_or_false(f"To print 'Hello world', you would write \"print(\"Hello World\")\"", True):
-  correct += 1
-  questions += 1
-else:
-  questions += 1
-  questions -= 1
-
+var = True
+vvar = 0
+while var:
+  if stb.true_or_false(f"To print 'Hello world', you would write \"print(\"Hello World\")\"", True):
+    correct += 1
+    questions += 1
+    vvar +=1
+  else:
+    questions += 1
+    questions -= 1
+    vvar +=1
   
-if stb.single_choice("How do you insert COMMENTS in Python code?", ["\#This is a comment", "//This is a comment", "/*This is a comment*/"], 0):
-  correct += 1
-  questions += 1
-else:
-  questions += 1
-  questions -= 1
+    
+  if stb.single_choice("How do you insert COMMENTS in Python code?", ["\#This is a comment", "//This is a comment", "/*This is a comment*/"], 0):
+    correct += 1
+    questions += 1
+    vvar +=1
+  else:
+    questions += 1
+    questions -= 1
+    vvar +=1
+
+  if vvar = 2:
+    var = False
 
 st.write(f"You have had {correct}/{questions} questions right.")
