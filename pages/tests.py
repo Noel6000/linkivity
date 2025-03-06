01828acd5cafbc816467b304b2c6c7ea8f4d2460
Noel6000 <noelsantiago.briand@gmail.com>
2025-03-06 13:24:07 +0100

    Update tests.py

--- pages/tests.py
+++ pages/tests.py
@@ -4,4 +4,6 @@ import random
 import time
 from openai import OpenAI
 
+st.header("Test your knowledge.")
+st.subheader(":warning::red[:green-background[This test will be used to classify your knowledge by companies]]:warning:")
 
