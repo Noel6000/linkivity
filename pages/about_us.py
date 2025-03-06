c8a860ff512348e5d08d8f5d3a41823d3015aa2d
Noel6000 <noelsantiago.briand@gmail.com>
2025-03-06 12:05:20 +0100

    Update about_us.py

--- pages/about_us.py
+++ pages/about_us.py
@@ -5,7 +5,7 @@ import time
 from openai import OpenAI
 
 st.header("About us")
-st.write("We are Baltasar, Andrés, Michael, Jiajia, Lilian, and Noël, a group of 8th grade students that have developed an alternative to linkedin for the Eureka Starter Days.")
+st.write("We are a group of 8th grade students that have developed an alternative to linkedin for the Eureka Starter Days. From right to left, and from back to front: Jiajia (researcher), Michael (researcher), Baltasar (founder, marketing expert), Noël (programmer), Lilian (Technical agent), and Andrés (Communications expert).")
 st.image("group.png")
 
 
