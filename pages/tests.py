1d3499eff10d2894371c7124885e52c7820fe735
Noel6000 <noelsantiago.briand@gmail.com>
2025-03-06 13:46:59 +0100

    Update tests.py

--- pages/tests.py
+++ pages/tests.py
@@ -1,6 +1,7 @@
 import streamlit as st
+import streamlit_book as stb
 import numpy as np
 import random
 import time
 
-st.true_or_false(f"How would you make python print {"Hello World"}", answer, success='Correct answer', error='Wrong answer', button='Check answer')
+stb.true_or_false(f"How would you make python print {"Hello World"}", answer, success='Correct answer', error='Wrong answer', button='Check answer')
