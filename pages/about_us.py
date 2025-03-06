4628cf4ac437ac0a64dcb77e28fd3dc0a1e01c0b
Noel6000 <noelsantiago.briand@gmail.com>
2025-03-06 11:51:19 +0100

    Update about_us.py

--- pages/about_us.py
+++ pages/about_us.py
@@ -4,4 +4,9 @@ import random
 import time
 from openai import OpenAI
 
+st.heder("About us")
 
+isClicked = st.button("mainpage", use_container_width=True)
+
+if isClicked :
+    st.switch_page("main_page.py")
