d8aaf6de74c4ac10fd638702c55416007958d641
Noel6000 <noelsantiago.briand@gmail.com>
2025-03-06 14:02:58 +0100

    Update tests.py

--- pages/tests.py
+++ pages/tests.py
@@ -9,14 +9,14 @@ questions = 0
 if stb.true_or_false(f"To print 'Hello world', you would write \"print(\"Hello World\")\"", True):
   correct += 1
   questions += 1
-if not stb.true_or_false(f"To print 'Hello world', you would write \"print(\"Hello World\")\"", True):
+else:
   questions += 1
 
-
+  
 if stb.single_choice("How do you insert COMMENTS in Python code?", ["\#This is a comment", "//This is a comment", "/*This is a comment*/"], 0):
   correct += 1
   questions += 1
-if not stb.single_choice("How do you insert COMMENTS in Python code?", ["\#This is a comment", "//This is a comment", "/*This is a comment*/"], 0):
+else:
   questions += 1
 
 st.write(f"You have had {correct}/{questions} questions right.")
