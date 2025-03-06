69f5650dc0485cb72d1f8b35b241d03754268f10
Noel6000 <noelsantiago.briand@gmail.com>
2025-03-06 13:28:27 +0100

    Update tests.py

--- pages/tests.py
+++ pages/tests.py
@@ -4,6 +4,9 @@ import random
 import time
 from openai import OpenAI
 
+isClicked = st.button("mainpage", use_container_width=True)
+if isClicked :
+    st.switch_page("main_page.py")
 st.header("Test your knowledge.")
 st.header(":warning:")
 st.subheader(":red[:blue-background[This test will be used to classify your knowledge by companies wishing to hire web developers.]]")
