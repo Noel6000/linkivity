88fa79e0f1ff822727045d6d58fee8ab8697bdae
Noel6000 <noelsantiago.briand@gmail.com>
2025-03-06 13:44:48 +0100

    Update tests.py

--- pages/tests.py
+++ pages/tests.py
@@ -2,4 +2,5 @@ import streamlit as st
 import numpy as np
 import random
 import time
-from openai import OpenAI
+
+st.true_or_false(f"How would you make python print {"Hello World"}", answer, success='Correct answer', error='Wrong answer', button='Check answer')
