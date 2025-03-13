9375f016b6c190d1ea49358f41dbce8e02927a5b
calvooooyqguwa <alvaroabaspal1@gmail.com>
2025-03-13 10:22:45 +0100

    Update about_us.py

--- pages/about_us.py
+++ pages/about_us.py
@@ -5,7 +5,8 @@ import time
 from openai import OpenAI
 
 st.header("About us")
-st.write("Our clothing brand consist of one product which is a LIFE shirt, it comes in diferent sizes, colours and editions. ")
+st.write("Our clothing brand consists of one product which is a LIFE shirt, it comes in diferent sizes, colours and editions. ")
+st.write("There is a special limited edition Mr.A shirt with Mr.A's signature.")
 st.subheader("Why are we sustainable?")
 st.write("aqui va el textaco de lilian")
 
