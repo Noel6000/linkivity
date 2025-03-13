08141387f870a5c26c986f877c1034ad31dcad24
Noel6000 <noelsantiago.briand@gmail.com>
2025-03-13 10:19:18 +0100

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -19,7 +19,7 @@ if "authenticated" not in st.session_state or not st.session_state.authenticated
 headers = st.context.headers
 button_container = st.container()
 with button_container:
-    col1, col2 = st.columns([1, 1])
+    col1, col2, col3 = st.columns([1, 1, 1])
     with col1:
         ModelIsClicked=st.button("About us",use_container_width=True)
     if ModelIsClicked:
@@ -28,3 +28,7 @@ with button_container:
         ModelIsClicked=st.button("login",use_container_width=True)
     if ModelIsClicked:
         st.switch_page("pages/login.py")
+    with col3:
+        ModelIsClicked=st.button("Shop", use_container_width=True)
+    if ModelIsClicked:
+        st.switch_page("pages/shop.py")
