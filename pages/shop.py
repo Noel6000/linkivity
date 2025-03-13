50614fa78502efed33d6c1f38140ec1e11559138
Noel6000 <noelsantiago.briand@gmail.com>
2025-03-13 10:21:03 +0100

    Update shop.py

--- pages/shop.py
+++ pages/shop.py
@@ -1,1 +1,10 @@
+import streamlit as st
+import numpy as np
+import random
+import time
 
+
+isClicked = st.button("mainpage", use_container_width=True)
+
+if isClicked :
+    st.switch_page("main_page.py")
