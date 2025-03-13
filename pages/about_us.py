3b29c0187b0ae1a9425d7243801d7f96537f5a36
Noel6000 <noelsantiago.briand@gmail.com>
2025-03-13 11:47:26 +0100

    Update about_us.py

--- pages/about_us.py
+++ pages/about_us.py
@@ -8,10 +8,10 @@ from openai import OpenAI
 custom_css = """
 <style>
     body {
-        background-color: #abe0ad;  
+        background-color: #6ade70;  
     }
     .stApp {
-        background-color: #abe0ad; 
+        background-color: #6ade70; 
     }
     .stButton>button {
         background-color: #4CAF50;
