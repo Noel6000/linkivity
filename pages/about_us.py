aea2fc5e9e6591b8ffe2956552ba817f33ee7b3c
Noel6000 <noelsantiago.briand@gmail.com>
2025-03-26 10:22:56 +0100

    Update about_us.py

--- pages/about_us.py
+++ pages/about_us.py
@@ -35,11 +35,11 @@ custom_css = """
 st.markdown(custom_css, unsafe_allow_html=True)
 
 st.header("About us")
-st.write("Our clothing brand consists of one product which is a LIFE shirt, it comes in different sizes, colors and editions. ")
-st.write("There is a special limited edition Mr.A shirt with Mr.A's signature.")
+st.write("Our clothing brand consists of one product, which is a LIFE shirt. It comes in different sizes, colors, and editions.")
+st.write("There is a special limited edition, the official Mr. A shirt with Mr. A's signature!")
 st.image("exclusive.png")
 st.subheader("Why are we sustainable?")
-st.write("We offer some handcrafted and local t-shirts to avoid sub-human conditions for workers, as “Wituka” collaborates with the Eden project which helps give good conditions to the workers. Additionally, all our products are done with 100% recycled materials, from old, discarded t-shirts. All our textile is locally produced, and our dyes, safe. With our T-Shirts for Everyone program, for every 30 € we earn at the end of the months we give five euros for Save The Children. With LIFE Products, choose to defend the workers right and escape the lie of fast fashion.")
+st.write("We offer some handcrafted and local t-shirts to avoid sub-human conditions for workers, as “Wituka” collaborates with the Eden project, which helps give good conditions to the workers. Additionally, all our products are made with 100% recycled materials, from old, discarded t-shirts. All our textiles are locally produced, and our dyes are safe. With our T-Shirts for Everyone program, for every €30 we earn at the end of the months, we give five euros to Save the Children. With LIFE Products, choose to defend the worker's right and escape the lie of fast fashion.")
 
 
 isClicked = st.button("Go back to the Main Page", use_container_width=True)
