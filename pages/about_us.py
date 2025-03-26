7379c6513e7c3d4abad0e3a84cdfd38073aa1a8d
Noel6000 <noelsantiago.briand@gmail.com>
2025-03-26 10:17:24 +0100

    Update about_us.py

--- pages/about_us.py
+++ pages/about_us.py
@@ -42,7 +42,7 @@ st.subheader("Why are we sustainable?")
 st.write("We offer some handcrafted and local t-shirts to avoid sub-human conditions for workers, as “Wituka” collaborates with the Eden project which helps give good conditions to the workers. Additionally, all our products are done with 100% recycled materials, from old, discarded t-shirts. All our textile is locally produced, and our dyes, safe. With our T-Shirts for Everyone program, for every 30 € we earn at the end of the months we give five euros for Save The Children. With LIFE Products, choose to defend the workers right and escape the lie of fast fashion.")
 
 
-isClicked = st.button("MAINPAGE", use_container_width=True)
+isClicked = st.button("Go back to the Main Page", use_container_width=True)
 
 if isClicked :
     st.switch_page("main_page.py")
