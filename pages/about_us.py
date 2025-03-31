fba98a63395943c49a5697ee17ddd0ce6179ef01
calvooooyqguwa <alvaroabaspal1@gmail.com>
2025-03-31 11:14:03 +0200

    Update about_us.py

--- pages/about_us.py
+++ pages/about_us.py
@@ -38,8 +38,8 @@ st.header("About us")
 st.subheader("Why are we sustainable?")
 st.write("We offer some handcrafted and local T-shirts to avoid sub-human conditions for workers by choosing a sustainable provider, Wituka.  Wituka collaborates with the Eden project, which helps give good conditions to the workers. All of the T-shirt we buy from Wituka are made with 100% organic cotton, which reduces the water used in the production of the T-shirt by 91%. At  the ed of each month, we give 10% of our remaining profits to Save The Children.")
 st.write("Choose LIFE Products to defend the worker's right and escape the lie of fast fashion!")
-st.write("There is a special limited edition, the official Mr. A shirt with Mr. A's signature!")
 st.image("exclusive.png")
+st.write("Image above: Exclusive LIFE T-shirt, with Mr. A's signature.")
 
 
 isClicked = st.button("Go back to the main page", use_container_width=True)
